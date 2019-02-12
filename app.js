3638ebab3a7d761672431576fba89b9450a71a26
henrynferg <henrynferg@gmail.com>
2019-02-11 19:33:48 -0800

    At midnight every day, debt data is updated

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
+const schedule = require('node-schedule');
+const moment = require('moment');
 var caller = require('./apiCaller');
 const app = new express();
 const port = 4040;
+
+var updateDebtData = function() {
+    caller.fetchData().then(function(result) {
+        app.get('/height', function(req, res) {
+            res.send({debt: result});
+        });
+    });
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -11,11 +21,9 @@ app.get('/', function(request, response){
     response.sendFile('.\\index.html', {root: root});
 });
 
-caller.fetchData().then(function(result) {
-    app.get('/height', function(req, res) {
-        res.send({debt: result});
-    });
-});
+updateDebtData();
+var jobDate = moment().startOf('day');
+schedule.scheduleJob(jobDate, updateDebtData);
 
 app.listen(port);
 console.log("Listening at port " + port);
